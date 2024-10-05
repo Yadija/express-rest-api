7d6305936ce89990890141b4d8c388e71d5188f6
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-10-06 00:12:02 +0800

    🚨test: make threads e2e test

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -2,12 +2,24 @@ import bcrypt from "bcrypt";
 // utils
 import users from "../src/utils/users.js";
 import tokens from "../src/utils/authentications.js";
+import threads from "../src/utils/threads.js";
 
 export const createTestUser = () => {
   const user = {
     id: "user-123",
     username: "johndoe",
     fullname: "John Doe",
+    password: bcrypt.hashSync("secret", 10),
+  };
+
+  users.push(user);
+};
+
+export const createAnotherTestUser = () => {
+  const user = {
+    id: "user-456",
+    username: "janedoe",
+    fullname: "Jane Doe",
     password: bcrypt.hashSync("secret", 10),
   };
 
@@ -23,4 +35,46 @@ export const removeAllTestUsers = () => {
 
 export const removeAllTestAuthentication = () => {
   tokens.length = 0;
+};
+
+export const createManyTestThreads = () => {
+  for (let i = 0; i < 5; i++) {
+    threads.push({
+      id: `thread-${i + 1}`,
+      content: `Hello World ${i + 1}`,
+      owner: "user-123",
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    });
+  }
+};
+
+export const createAnotherManyTestThreads = () => {
+  for (let i = 0; i < 5; i++) {
+    threads.push({
+      id: `thread-${i + 6}`,
+      content: `Hello World ${i + 6}`,
+      owner: "user-456",
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    });
+  }
+};
+
+export const createTestThread = () => {
+  threads.push({
+    id: "thread-123",
+    content: "Hello World",
+    owner: "user-123",
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  });
+};
+
+export const getTestThread = async () => {
+  return threads.find((thread) => thread.id === "thread-123");
+};
+
+export const removeAllTestThreads = () => {
+  threads.length = 0;
 };
