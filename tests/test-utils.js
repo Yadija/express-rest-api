76cb1ce7542d60a66d8453c0a84d5d10315a2c12
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-10-08 09:06:44 +0800

    🚨test: make users e2e test

---
+++ tests/test-utils.js
@@ -0,0 +1,36 @@
+import bcrypt from "bcrypt";
+// application
+import pool from "../src/application/database.js";
+
+export const createTestUser = async () => {
+  const user = {
+    id: "user-123",
+    username: "johndoe",
+    fullname: "John Doe",
+    password: bcrypt.hashSync("secretpassword", 10),
+  };
+
+  const query = {
+    text: "INSERT INTO users VALUES ($1, $2, $3, $4)",
+    values: [user.id, user.username, user.password, user.fullname],
+  };
+
+  await pool.query(query);
+};
+
+export const getTestUserById = async () => {
+  const query = {
+    text: "SELECT * FROM users WHERE id = $1",
+    values: ["user-123"],
+  };
+
+  const { rows } = await pool.query(query);
+  return rows[0];
+};
+
+export const removeAllTestUsers = async () => {
+  const query = {
+    text: "DELETE FROM users WHERE 1=1",
+  };
+  await pool.query(query);
+};
