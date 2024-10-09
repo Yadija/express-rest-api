7a8ce401a83f89b0bee4b44364c00de89770c70d
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-10-09 10:20:57 +0800

    ⚒️build: setup database

---
+++ src/application/database.js
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import { logger } from "./logging.js";
+
+export const prismaClient = new PrismaClient({
+  log: [
+    { emit: "event", level: "query" },
+    { emit: "event", level: "error" },
+    { emit: "event", level: "info" },
+    { emit: "event", level: "warn" },
+  ],
+});
+
+prismaClient.$on("error", (error) => {
+  logger.error(error.message);
+});
+
+prismaClient.$on("warn", (warn) => {
+  logger.warn(warn.message);
+});
+
+prismaClient.$on("info", (info) => {
+  logger.info(info.message);
+});
+
+prismaClient.$on("query", (query) => {
+  logger.info(
+    `(duration: ${query.duration}ms) query: ${query.query}, params: ${JSON.stringify(query.params)}`,
+  );
+});
