439064d79ec0ae8d976c98c47f8aac9c4a4c262c
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-10-11 14:46:57 +0800

    🚨test: make threads e2e test

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -13,6 +13,17 @@ export const createTestUser = async () => {
   });
 };
 
+export const createAnotherTestUser = async () => {
+  await prismaClient.user.create({
+    data: {
+      id: "user-456",
+      username: "janedoe",
+      password: await bcrypt.hash("secretpassword", 10),
+      fullname: "Jane Doe",
+    },
+  });
+};
+
 export const getTestUserById = async () => {
   const user = await prismaClient.user.findUnique({
     where: { id: "user-123" },
@@ -27,4 +38,48 @@ export const removeAllTestUsers = async () => {
 
 export const removeAllTestAuthentication = async () => {
   await prismaClient.token.deleteMany({});
+};
+
+export const createManyTestThreads = async () => {
+  for (let i = 0; i < 5; i++) {
+    await prismaClient.thread.create({
+      data: {
+        content: `Hello World ${i}`,
+        owner: "user-123",
+      },
+    });
+  }
+};
+
+export const createAnotherManyTestThreads = async () => {
+  for (let i = 0; i < 5; i++) {
+    await prismaClient.thread.create({
+      data: {
+        content: `Hello World ${i}`,
+        owner: "user-456",
+      },
+    });
+  }
+};
+
+export const createTestThread = async () => {
+  await prismaClient.thread.create({
+    data: {
+      id: "thread-123",
+      content: "Hello World",
+      owner: "user-123",
+    },
+  });
+};
+
+export const getTestThread = async () => {
+  const thread = await prismaClient.thread.findUnique({
+    where: { id: "thread-123" },
+  });
+
+  return thread;
+};
+
+export const removeAllTestThreads = async () => {
+  await prismaClient.thread.deleteMany({});
 };
