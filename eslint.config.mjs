29a8e3c38c55f8a01aac5704cebdf4ba0f3bc957
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-10-16 08:57:23 +0800

    ⚒️build: install and setup eslint with jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import jest from "eslint-plugin-jest";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
@@ -9,4 +10,12 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  {
+    files: ["tests/**/*.test.{js,jsx,ts,tsx}"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off",
+    },
+  },
 ];
