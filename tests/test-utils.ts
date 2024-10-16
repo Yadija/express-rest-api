dbf4cd50f9ba4bdf461fd79dae29e5d70475ec59
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-10-16 10:21:05 +0800

    🚨test: make users e2e test

---
+++ tests/test-utils.ts
@@ -0,0 +1,30 @@
+import bcrypt from "bcrypt";
+// application
+import { prismaClient } from "../src/application/database";
+
+export const createTestUser = async () => {
+  await prismaClient.user.create({
+    data: {
+      id: "user-123",
+      username: "johndoe",
+      password: await bcrypt.hash("secretpassword", 10),
+      fullname: "John Doe",
+    },
+  });
+};
+
+export const getTestUserById = async () => {
+  const user = await prismaClient.user.findUnique({
+    where: { id: "user-123" },
+  });
+
+  if (!user) {
+    throw new Error("cannot find user");
+  }
+
+  return user;
+};
+
+export const removeAllTestUsers = async () => {
+  await prismaClient.user.deleteMany({});
+};
---
+++ tests/users.test.ts
@@ -0,0 +1,111 @@
+import supertest from "supertest";
+import { web } from "../src/application/web";
+import {
+  createTestUser,
+  getTestUserById,
+  removeAllTestUsers,
+} from "./test-utils";
+
+describe("POST /users", () => {
+  afterEach(async () => {
+    await removeAllTestUsers();
+  });
+
+  it("should response 201 and create new user", async () => {
+    const result = await supertest(web).post("/users").send({
+      username: "johndoe",
+      password: "secretpassword",
+      fullname: "John Doe",
+    });
+
+    expect(result.status).toBe(201);
+    expect(result.body.status).toBe("success");
+    expect(result.body.message).toBeDefined();
+    expect(result.body.timestamp).toBeDefined();
+
+    expect(result.body.data.user).toBeDefined();
+    expect(result.body.data.user.id).toBeDefined();
+    expect(result.body.data.user.username).toBe("johndoe");
+    expect(result.body.data.user.fullname).toBe("John Doe");
+  });
+
+  it("should response 400 and reject if request is invalid", async () => {
+    const result = await supertest(web).post("/users").send({
+      username: "",
+      password: "",
+      fullname: "",
+    });
+
+    expect(result.status).toBe(400);
+    expect(result.body.status).toBe("fail");
+    expect(result.body.message).toBeDefined();
+    expect(result.body.timestamp).toBeDefined();
+    expect(result.body.path).toBe("/users");
+  });
+
+  it("should response 400 and reject if username already registered", async () => {
+    let result = await supertest(web).post("/users").send({
+      username: "johndoe",
+      password: "secretpassword",
+      fullname: "John Doe",
+    });
+
+    expect(result.status).toBe(201);
+    expect(result.body.status).toBe("success");
+    expect(result.body.message).toBeDefined();
+    expect(result.body.timestamp).toBeDefined();
+
+    expect(result.body.data.user).toBeDefined();
+    expect(result.body.data.user.id).toBeDefined();
+    expect(result.body.data.user.username).toBe("johndoe");
+    expect(result.body.data.user.fullname).toBe("John Doe");
+
+    result = await supertest(web).post("/users").send({
+      username: "johndoe",
+      password: "secretpassword",
+      fullname: "John Doe",
+    });
+
+    expect(result.status).toBe(400);
+    expect(result.body.status).toBe("fail");
+    expect(result.body.message).toBeDefined();
+    expect(result.body.timestamp).toBeDefined();
+    expect(result.body.path).toBe("/users");
+  });
+});
+
+describe("GET /users/:userId", () => {
+  beforeAll(async () => {
+    await createTestUser();
+  });
+
+  afterAll(async () => {
+    await removeAllTestUsers();
+  });
+
+  it("should response 200 and get user", async () => {
+    const testUser = await getTestUserById();
+
+    const result = await supertest(web).get(`/users/${testUser.id}`);
+
+    expect(result.status).toBe(200);
+    expect(result.body.status).toBe("success");
+    expect(result.body.message).toBeDefined();
+    expect(result.body.timestamp).toBeDefined();
+
+    expect(result.body.data.user).toBeDefined();
+    expect(result.body.data.user.id).toBe("user-123");
+    expect(result.body.data.user.username).toBe("johndoe");
+    expect(result.body.data.user.fullname).toBe("John Doe");
+  });
+
+  it("should response 404 and cannot get user", async () => {
+    const result = await supertest(web).get("/users/not-found");
+
+    expect(result.status).toBe(404);
+    expect(result.body.status).toBe("fail");
+    expect(result.body.message).toBeDefined();
+    expect(result.body.timestamp).toBeDefined();
+    expect(result.body.path).toBe("/users/not-found");
+  });
+});
